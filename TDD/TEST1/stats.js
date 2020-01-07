9fd63b101ebce844ca77fc6ecc6b83fe0a2f33df
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-01-07 16:45:01 +0900

    TDD study #1

---
+++ TDD/TEST1/stats.js
@@ -0,0 +1,11 @@
+exports.max = numbers => {
+    Math.max(...numbers);
+    // let result = numbers[0];
+    // numbers.forEach(n => {
+    //     if (n > result) {
+    //         result = n;
+    //     }
+    // });
+    // return result;
+};
+exports.min = numbers => Math.min(...numbers);
