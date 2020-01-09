280c08bb9ba30f84a247d6efe3d7ed3032332b2f
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-01-09 16:07:37 +0900

    finish post and get about TDD

--- TDD/TEST1/index.js
+++ TDD/TEST1/index.js
@@ -1,10 +1,37 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
+
+app.post('/login', (req, res) => {
+    const {email, password} = req.body;
+
+    if (!req.body) {
+        res.status(400).send('Bad Request');
+        return;
+    }
+
+    if(email !== 'rdd9223@gmail.com' || password !== 'qwerty') {
+        res.status(401).send('Unauthorized');
+        return;
+    }
+
+    res.send({
+        id: 'haaa-am',
+        email: email,
+        name: '김개똥',
+        age: 20
+    })
+})
 
 module.exports = app.listen(3000, () => {
     console.log('Awesome TDD + ES6 app listening on port 3000!');
--- TDD/TEST1/index.spec.js
+++ TDD/TEST1/index.spec.js
@@ -21,3 +21,29 @@ describe('GET /', () => {
             });
     });
 });
+
+describe('POST /login', () => {
+    it('should respond with profile', (done) => {
+        request(app)
+            .post('/login')
+            .send({
+                email: 'rdd9223@gmail.com',
+                password: 'qwerty'
+            })
+            .expect(200)
+            .end((err, res) => {
+                if (err) {
+                    done(err);
+                    return;
+                }
+
+                expect(res.body).has.all.keys([
+                    'id', 'email', 'name', 'age'
+                ]);
+                expect(res.body.id).to.equal('haaa-am');
+                expect(res.body.name).to.equal('김개똥');
+                expect(res.body.age).to.equal(20);
+                done();
+            });
+    })
+})
