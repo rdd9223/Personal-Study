a2ae4a47a59c1a7fbbf9e2eefb0f5487229dffdf
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-01-14 00:36:21 +0900

    admit elements

--- React/jsx/my-app/src/index.js
+++ React/jsx/my-app/src/index.js
@@ -4,7 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 이거는 원래 있던거
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+// 함수 처리
+const formatName = name => name.firstName + " " + name.lastName;
+
+// 이름객체
+const name = {
+    firstName: "영우",
+    lastName: "강"
+};
+const element = <h1>Hello, {formatName(name)}!</h1>
+
+ReactDOM.render(
+    element,
+    document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
