322b547e4393b975a4d716719ad68cd012268786
YeongWoooo <46023074+rdd9223@users.noreply.github.com>
2020-01-18 23:46:56 +0900

    add friends

--- React/jsx/my-app/src/Chapter11/UserProfileList.js
+++ React/jsx/my-app/src/Chapter11/UserProfileList.js
@@ -12,6 +12,7 @@ class UserProfileList extends React.Component {
                 { id: 3, name: '수영', image: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png', job: 'CEO, Amazon' },
                 { id: 4, name: '연중', image: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png', job: 'CEO, Apple' },
                 { id: 5, name: '건욱', image: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png', job: 'Actor' },
+                { id: 7, name: '한주', image: 'ㄹㄹㄹ', job: '놈팽이' },
                 { id: 6, name: '성민', image: 'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png', job: 'Actress' }
             ]
         }
