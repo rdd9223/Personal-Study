65c31a94ca31843163473bee2612102fe7866d05
YeongWoooo <rdd9223@gmail.com>
2021-05-13 15:07:52 +0900

    Refactor: 토글기능 추가

--- nomadcoder/index.js
+++ nomadcoder/index.js
@@ -1,26 +1,25 @@
-const title = document.querySelector("#title"); // id를 기반으로 찾아서 첫번째 요소를 반환함
-// title.innerHTML = "Hi! From JS";
-// title.style.color = "red";
-// document.title = "I own you now";
+const title = document.querySelector("#title");
 
-const BASE_COLOR = "rgb(52, 73, 94)"; // html에서는 rgb로 반환되기 때문에 rgb를 넣어야 동작한다.
-const OTHER_COLOR = "#7f8c8d";
+const CLICKED_CLASS = "clicked";
 
 function handleClick() {
-  const currentColor = title.style.color;
-  if (currentColor === BASE_COLOR) {
-    title.style.color = OTHER_COLOR;
-  } else {
-    title.style.color = BASE_COLOR;
-  }
+  // // 해당 클래스에 clicked라는 클래스가 포함되어 있는지 확인
+  // const hasClass = title.classList.contains(CLICKED_CLASS);
+
+  // if (hasClass) {
+  //   // 포함되어있다면 clicked를 삭제!
+  //   title.classList.remove(CLICKED_CLASS);
+  // } else {
+  //   // 포함되어있지 않다면 clicked를 추가!
+  //   title.classList.add(CLICKED_CLASS);
+  // }
+
+  // 위 코드와 완전히 똑같은 기능을 한다.
+  title.classList.toggle(CLICKED_CLASS);
 }
 
 function init() {
-  title.style.color = BASE_COLOR;
-  title.addEventListener("click", handleClick); // mouseenter를 넣으면 마우스가 들어갈때마다 변경된다.
+  title.addEventListener("click", handleClick);
 }
 
 init();
-// handleResize를 ()를 붙이면 즉시실행, 안붙이면 선택적 실행이다.
-// 브라우저의 크기를 변경하면 호출된다.
-// window.addEventListener("resize", handleResize);
