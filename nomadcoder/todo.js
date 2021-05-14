f0e9621e0436449d68587de557638fe1b9cd2e5e
YeongWoooo <rdd9223@gmail.com>
2021-05-14 11:20:48 +0900

    Feat: 로컬 스토리지에 투두 저장

--- nomadcoder/todo.js
+++ nomadcoder/todo.js
@@ -4,7 +4,33 @@ const toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
+const toDos = [];
+
+function saveToDos() {
+  localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
+}
+
+function paintToDo(text) {
+  const toDoObj = {
+    text: text,
+    id: newId, // 인덱스를 동적으로 넣어줌
+  };
+
+  toDos.push(toDoObj);
+  saveToDos();
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = toDoInput.value;
+  paintToDo(currentValue);
+  toDoInput.value = ""; // enter를 누르면 값을 초기화 시킴
+}
+
 function loadToDos() {
+  const loadedToDos = localStorage.getItem(TODOS_LS);
+  if (loadedToDos !== null) {
+  }
 }
 
 function init() {
